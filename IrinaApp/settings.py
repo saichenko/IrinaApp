77ed12dd4400f7f2a71d4be3544a8eb1f3172037
RichardAnderson8 <reallinuxforlinux@gmail.com>
2020-05-17 19:51:23 +0700

    pushing

--- IrinaApp/settings.py
+++ IrinaApp/settings.py
@@ -11,8 +11,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 
-SECRET_KEY = os.environ['SECRET_KEY']
-
+#SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '2dihl^)!0(47@id8wyuqmk03b!7c&$i#57073qrdm8ch#2h4@1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn locallibrary.wsgi --log-file -
+web: gunicorn IrinaApp.wsgi --log-file -
